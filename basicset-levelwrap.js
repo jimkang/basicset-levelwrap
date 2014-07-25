e67a43f0e5c2432f77eb3f5a7afa4612e9ae3cbb
Jim Kang <jimkang@gmail.com>
2014-07-25 08:59:46 -0400

    Refactored levelwrap to not use `this` at all so that methods can be safely used with modules like queue-async without having to call `bind` on every method.

--- basicset-levelwrap.js
+++ basicset-levelwrap.js
@@ -1,59 +1,57 @@
 function createLevelWrap(leveldbFilePath) {
   var levelup = require('level');
 
-  var levelwrap = {
-    db: levelup(leveldbFilePath, {valueEncoding: 'json'}),
-    nsDelimiter: '!',
-    nsEndRangeDelimiter: '\xff' // ÿ
-  };
+  var db = levelup(leveldbFilePath, {valueEncoding: 'json'});
+  var nsDelimiter = '!';
+  var nsEndRangeDelimiter = '\xff'; // ÿ
 
   // If the key can't be found, the done callback will be passed an empty array. 
   // If there is another error, the done callback will be passed null.
-  levelwrap.getObject = function getObject(id, docId, done) {
-    var key = this.getObjectKey(id, docId);
-    this.db.get(key, done);
+  function getObject(id, docId, done) {
+    var key = getObjectKey(id, docId);
+    db.get(key, done);
   };
 
   // doc (an id) is assumed to be one of the object properties.
-  levelwrap.saveObject = function saveObject(obj, done) {
-    var key = this.getObjectKey(obj.id, obj.doc);
-    this.db.put(key, obj, done);
+  function saveObject(obj, done) {
+    var key = getObjectKey(obj.id, obj.doc);
+    db.put(key, obj, done);
   };
 
-  levelwrap.deleteObject = function deleteObject(id, docId, done) {
-    var key = this.getObjectKey(id, docId);
-    this.db.del(key, {}, done);
+  function deleteObject(id, docId, done) {
+    var key = getObjectKey(id, docId);
+    db.del(key, {}, done);
   };
 
-  levelwrap.saveDoc = function saveDoc(doc, done) {
-    var key = this.getDocKey(doc.id);
-    this.db.put(key, doc, done);
+  function saveDoc(doc, done) {
+    var key = getDocKey(doc.id);
+    db.put(key, doc, done);
   };
 
-  levelwrap.getDoc = function getDoc(id, done) {
-    var key = this.getDocKey(id);
-    this.db.get(key, done);
+  function getDoc(id, done) {
+    var key = getDocKey(id);
+    db.get(key, done);
   };
 
-  levelwrap.sanitizeKeySegment = function sanitizeKeySegment(key) {
-    return key.replace(this.nsDelimiter, '');
+  function sanitizeKeySegment(key) {
+    return key.replace(nsDelimiter, '');
   };
 
-  levelwrap.getObjectKey = function getObjectKey(id, docId) {
-    var cleanId = this.sanitizeKeySegment(id);
-    var cleanDocId = this.sanitizeKeySegment(docId);
-    var key = 's' + this.nsDelimiter + cleanDocId + this.nsDelimiter + cleanId;  
+  function getObjectKey(id, docId) {
+    var cleanId = sanitizeKeySegment(id);
+    var cleanDocId = sanitizeKeySegment(docId);
+    var key = 's' + nsDelimiter + cleanDocId + nsDelimiter + cleanId;  
     return key;
   };
 
-  levelwrap.getDocKey = function getDocKey(id) {
-    var cleanId = this.sanitizeKeySegment(id);
-    var key = 'd' + this.nsDelimiter + cleanId;
+  function getDocKey(id) {
+    var cleanId = sanitizeKeySegment(id);
+    var key = 'd' + nsDelimiter + cleanId;
     return key;
   };
 
-  levelwrap.getDocIdFromKey = function getDocIdFromKey(key) {
-    var parts = key.split(this.nsDelimiter);
+  function getDocIdFromKey(key) {
+    var parts = key.split(nsDelimiter);
     var id = null;
     if (parts.length > 1) {
       id = parts[1];
@@ -61,28 +59,35 @@ function createLevelWrap(leveldbFilePath) {
     return id;
   };
 
-  levelwrap.getRangeForObjectsInDoc = function getRangeForObjectsInDoc(docId) {
-    var cleanDocId = this.sanitizeKeySegment(docId);
+  function getRangeForObjectsInDoc(docId) {
+    var cleanDocId = sanitizeKeySegment(docId);
     return [
-      's' + this.nsDelimiter + cleanDocId + this.nsDelimiter,
-      's' + this.nsDelimiter + cleanDocId + this.nsEndRangeDelimiter
+      's' + nsDelimiter + cleanDocId + nsDelimiter,
+      's' + nsDelimiter + cleanDocId + nsEndRangeDelimiter
     ];
   };
 
-  levelwrap.getDocObjectStream = function getDocObjectStream(docId) {
-    var range = this.getRangeForObjectsInDoc(docId);
-    return this.db.createValueStream({
+  function getDocObjectStream(docId) {
+    var range = getRangeForObjectsInDoc(docId);
+    return db.createValueStream({
       start: range[0],
       end: range[1]
     });
   };
 
-  levelwrap.close = function close(done) {
-    this.db.close(done);
+  function close(done) {
+    db.close(done);
   };
 
-  return levelwrap;
+  return {
+    getObject: getObject,
+    saveObject: saveObject,
+    deleteObject: deleteObject,
+    saveDoc: saveDoc,
+    getDoc: getDoc,
+    getDocObjectStream: getDocObjectStream,
+    close: close
+  };
 }
 
 module.exports.createLevelWrap = createLevelWrap;
-
